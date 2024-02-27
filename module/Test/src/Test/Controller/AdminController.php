485610e27c824c3963addb95e3c9dea1007903b9
朴昰成（パク・ハソン） <spredempt@gmail.com>
2024-02-27 16:47:36 +0900

    Change Exam-Regist Front Function

--- module/Test/src/Test/Controller/AdminController.php
+++ module/Test/src/Test/Controller/AdminController.php
@@ -248,6 +248,7 @@ class AdminController extends AbstractActionController
 		/* 修正後 */
 		$breadcrumb = array("応募者状況管理", "試験登録");
 		/* ここまで */
+
     $this->layout()->breadcrumb = json_encode($breadcrumb);
 
     //view==============================================================
@@ -349,6 +350,7 @@ class AdminController extends AbstractActionController
 		/* 修正後 */
 		$breadcrumb = array("応募者状況管理", "試験登録", "確認");
 		/* ここまで */
+		
     $this->layout()->breadcrumb = json_encode($breadcrumb);
 
     //view==============================================================
@@ -380,6 +382,7 @@ class AdminController extends AbstractActionController
 		/* 修正後 */
 		$breadcrumb = array("応募者状況管理", "試験一覧");
 		/* ここまで */
+		
     $this->layout()->breadcrumb = json_encode($breadcrumb);
 
     //view==============================================================

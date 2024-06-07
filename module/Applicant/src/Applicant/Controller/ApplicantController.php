cda937de39bfb8c86f63f708126ec6a21a325518
IS2023 ジョン·ソクウォン(丁錫圓) <jsrop07@gmail.com>
2024-06-07 20:44:50 +0900

    finaltest2_0607

--- module/Applicant/src/Applicant/Controller/ApplicantController.php
+++ module/Applicant/src/Applicant/Controller/ApplicantController.php
@@ -281,7 +281,7 @@ class ApplicantController extends AbstractActionController
 			}
 		}
 
-		$percentPoint = ceil($get_point/$diagnosisInfo["point_total"]*100);
+		// $percentPoint = ceil($get_point/$diagnosisInfo["point_total"]*100);
 
 		$selectedRank = [];
 		$resultPoints = explode(',', $diagnosisInfo["result_points"]);
@@ -336,7 +336,7 @@ class ApplicantController extends AbstractActionController
 		if($submit_post=='btn_submit'){
 			$sqlWhere["idx"] = $examRecordInfo['idx'];
 			$sqlSet["answer_data"] = $answer_data;
-			$sqlSet["get_point"] = $percentPoint;
+			$sqlSet["get_point"] = $get_point;
 			$sqlSet['rank']=$recordRank;
 			$sqlSet['diagnosis_comment']=$recordExamResult;
 
